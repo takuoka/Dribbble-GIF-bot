13a309e3a2360a76646677f77d634f31f79e56c5
Takuya.Okamoto <blackn.red42@gmail.com>
2015-07-31 12:08:41 +0900

    add title & page url to html parsing

--- lib/dribbble_gif.ex
+++ lib/dribbble_gif.ex
@@ -1,31 +1,36 @@
 defmodule DribbbleGif do
 
-  # dribbleのURLももってくる
-  # 重複しない
+  # ✔ dribbleのURLももってくる
+  # ✔ タイトルも取ってくる
+  # 重複しない→TLとってくる
   # 定期ツイート
+  # All Timeもとりはじめる
+
+  @max_page 20
 
   def main do
-    #   random_gif_tweet
-
-    IO.puts "test.."
-    page_num = DribbbleGif.Util.random_num(10)
-    gif_url = DribbbleGif.GetGifUrl.fetch_random_gif_url(page_num)
-    IO.puts gif_url
+    #   IO.puts "test.."
+    #   page_num = DribbbleGif.Util.random_num(@max_page)
+    #   {title, link_url, gif_url} = DribbbleGif.GetGifUrl.fetch_random_gif_url(page_num)
+    #   IO.puts gif_url
+    random_gif_tweet
   end
 
   def random_gif_tweet do
       IO.puts("---- random_gif_tweet ----")
-      page_num = DribbbleGif.Util.random_num(10)
-      gif_url = DribbbleGif.GetGifUrl.fetch_random_gif_url(page_num)
-      IO.puts "🍓 " <> gif_url
-      IO.puts "uploading..."
-      tweet_gif("test", gif_url)
+      page_num = DribbbleGif.Util.random_num(@max_page)
+      {title, link_url, gif_url} = DribbbleGif.GetGifUrl.fetch_random_gif_url(page_num)
+      status = title <> " " <> link_url
+      IO.puts "💬 " <> status
+      tweet_gif(status, gif_url)
       IO.puts "------- tweeted. ---------"
   end
 
   def tweet_gif(message, url) do
+      IO.puts "Downloading image..."
        {:ok, res} = HTTPoison.get(url)
        encoded_image = Base.encode64(res.body)
+       IO.puts "Uploading..."
        ExTwitter.API.Tweets.upload_tweet(message, encoded_image)
   end
 end
--- lib/get_gif_url.ex
+++ lib/get_gif_url.ex
@@ -3,6 +3,7 @@ defmodule DribbbleGif.GetGifUrl do
   require Floki
   require Timex
 
+  @dribbble_url "https://dribbble.com"
   @dribbble_gif_now_popular_url "https://dribbble.com/shots?list=animated"
 
   def fetch_random_gif_url(page) do
@@ -14,20 +15,35 @@ defmodule DribbbleGif.GetGifUrl do
   end
 
   defp extract_random_gif_url(body) do
-      feeds = Floki.find(body, ".dribbble-img")
-      [_|feeds] = feeds # remove first element (empty start)
-      # get random feed element
-      feeds = List.to_tuple(feeds)
-      random_index = DribbbleGif.Util.random_num(tuple_size(feeds)) - 1
-      feed = elem(feeds, random_index)
-      extract_gif_url(feed)
+    feeds = Floki.find(body, ".dribbble-img")
+    [_|feeds] = feeds # remove first element (empty start)
+    # get random feed element
+    feeds = List.to_tuple(feeds)
+    random_index = DribbbleGif.Util.random_num(tuple_size(feeds)) - 1
+    feed = elem(feeds, random_index)
+    # parse html
+    link_url = extract_link(feed)
+    gif_url = extract_gif_url(feed)
+    title = extract_title(feed)
+    {title, link_url, gif_url}
+  end
+
+  defp extract_title(feed_elem) do
+       {_, _, children_nodes} = feed_elem
+       children_nodes |> List.first |> elem(2) |> List.first |> elem(1) |> List.last |> elem(1)
+  end
+
+  defp extract_link(feed_elem) do
+    {_, _, children_nodes} = feed_elem
+    link_url =  children_nodes |> List.first |> elem(1) |> List.last |> elem(1)
+    link_url = @dribbble_url <> link_url
   end
 
   defp extract_gif_url(feed_elem) do
-      # get url
-      {tag_name, attributes, children_nodes} = feed_elem
-      gifUrl = children_nodes |> List.first |> elem(2) |> List.first |> elem(2) |> List.first |> elem(1) |> List.first |> elem(1)
-      # _teaser をけす
-      String.slice(gifUrl, 0, String.length(gifUrl) - 11) <> ".gif"
+    # get url
+    {_, _, children_nodes} = feed_elem
+    gifUrl = children_nodes |> List.first |> elem(2) |> List.first |> elem(2) |> List.first |> elem(1) |> List.first |> elem(1)
+    # remove "_teaser"
+    String.slice(gifUrl, 0, String.length(gifUrl) - 11) <> ".gif"
   end
 end
